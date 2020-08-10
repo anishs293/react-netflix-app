388159500922533eaece2114cd6647ccad316895
Anish Singh <anishs293@outlook.com>
2020-08-11 04:46:45 +0530

    Updates

---
+++ precache-manifest.c1ea81faebbb539520e4e2e27778f44c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c1bfccd79ea532639b11fc9e48b77b",
+    "url": "/react-netflix-app/index.html"
+  },
+  {
+    "revision": "30c4b57513ca4fae94f8",
+    "url": "/react-netflix-app/static/css/main.80ad6c3d.chunk.css"
+  },
+  {
+    "revision": "eebe712befafd8dae39b",
+    "url": "/react-netflix-app/static/js/2.2a8aa4b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-netflix-app/static/js/2.2a8aa4b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c4b57513ca4fae94f8",
+    "url": "/react-netflix-app/static/js/main.7592aa92.chunk.js"
+  },
+  {
+    "revision": "061a8312c378eb66b0f4",
+    "url": "/react-netflix-app/static/js/runtime-main.7b652e97.js"
+  }
+]);
